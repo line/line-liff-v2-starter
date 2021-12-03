6edd02d852139f841c912b6f39358dbc9ed4c4de
hiroki.zenigami <hiroki.zenigami@linecorp.com>
2021-12-03 18:24:15 +0900

    Fix env vars in vanilla

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -3,7 +3,7 @@ import liff from '@line/liff'
 
 document.addEventListener("DOMContentLoaded", function() {
   liff
-    .init({ liffId: process.env.LIFF_ID || '' })
+    .init({ liffId: process.env.LIFF_ID })
     .then(() => {
         console.log("Success! you can do something with LIFF API here.")
     })
