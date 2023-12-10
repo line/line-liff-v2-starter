bfde6b1c10ff6724282581713b8504f1373218ae
jschang19 <wwwsean13579@gmail.com>
2023-12-10 20:15:51 +0800

    refactor: update for Nuxt 3 usages
    - Rewrote nuxt.config for Nuxt 3 configuration
    - Improved typescript support for IDE by adding `tsconfig.json`
    - Refactored with Vue 3 composition API usage

---
+++ src/nuxtjs/nuxt.config.ts
@@ -0,0 +1,28 @@
+import pkg from './package.json'
+
+export default defineNuxtConfig({
+  app:{
+    head: {
+    title: 'LIFF Starter',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  },
+  plugins:[],
+  runtimeConfig: {
+    public: {
+      LIFF_ID: process.env.LIFF_ID,
+      VERSION: pkg.version || '0.1.0',
+    },
+  },
+})
